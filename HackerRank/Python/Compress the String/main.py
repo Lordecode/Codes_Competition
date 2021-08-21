f08a00250fc150ceb255844c234837f061889bd5
Eduardo <eduardo.hbds@gmail.com>
2021-08-20 21:26:22 -0300

    Update

--- HackerRank/Python/Compress the String/main.py
+++ HackerRank/Python/Compress the String/main.py
@@ -1,9 +1,16 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 value = input()
-num = 0 
-for i in value:
-    print(i)
-    if i == "a":
-        num +=1
+count = 0 
+a = value[0]
 
-print("num:"+ str(num))
+for i in value:
+    #print(i, " ", a)
+    if i != a or i == value[len(value)-1]:
+        count +=1
+        print("(",a ,",", str(count),")",end=" ")
+        count=0
+        a=i
+    else:
+        count +=1
+
+
